b4e5c986e4ce73a9e1ac6adfa6e9d682d054429b
johnyheera <johnyheera.sk@gmail.com>
2018-05-10 09:55:36 -0400

    project setup completed

---
+++ backend/src/main/java/jos/backend/App.java
@@ -0,0 +1,13 @@
+package jos.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
